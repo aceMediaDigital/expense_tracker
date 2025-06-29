5e8cd72237b36614b5595627fc70acba48bfa790
amaqanda <acedesigns123@gmail.com>
2025-06-29 20:19:30 +0200

    init splash/welcome screens

---
+++ lib/screens/splash/intro_screen.dart
@@ -0,0 +1,25 @@
+/* =======================================================
+ *
+ * Created by anele on 29/06/2025.
+ *
+ * @anele_ace
+ *
+ * =======================================================
+ */
+
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF58BC6B),
+      body: Center(
+        child: Text('data'),
+      ),
+    );
+  }
+}
